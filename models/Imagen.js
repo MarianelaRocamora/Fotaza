93ee38d0c20fd3c54a6db8656aafe72cdf9a2b5b
Marianela <rocamoramarianela86@gmail.com>
2026-05-18 19:41:31 -0300

    Agregar modelos de Imagen, Publicacion y Etiqueta con sus respectivas relaciones en Sequelize

---
+++ models/Imagen.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db/sequelize');
+
+const Imagen = sequelize.define('imagen', {
+    id_imagen: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    foto: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    ancho: DataTypes.INTEGER,
+
+    altura: DataTypes.INTEGER,
+
+    licencia: {
+        type: DataTypes.STRING(20),
+        defaultValue: 'sin_copyright',
+        validate: { isIn: [['copyright', 'sin_copyright']] }
+    },
+    marca_de_agua: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    texto_marca: DataTypes.STRING(200),
+    
+    fecha_subida: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    estado: {
+        type: DataTypes.STRING(20),
+        defaultValue: 'activo'
+    }
+}, {
+    tableName: 'imagen',
+    timestamps: false
+});
+
+module.exports = Imagen;
---
+++ models/Publicacion.js
@@ -0,0 +1,57 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db/sequelize');
+const Usuario = require('./Usuario');
+
+const Publicacion = sequelize.define('publicacion', {
+    id_publicacion: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    titulo: {
+        type: DataTypes.STRING(200),
+        allowNull: false
+    },
+
+    descripcion: DataTypes.TEXT,
+
+    fecha_publicacion: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    
+    estado: {
+        type: DataTypes.STRING(20),
+        defaultValue: 'activo'
+    },
+    comentarios_abiertos: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    },
+    id_creador: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+}, {
+    tableName: 'publicacion',
+    timestamps: false
+});
+
+Publicacion.belongsTo(Usuario, { foreignKey: 'id_creador', as: 'creador' });
+Usuario.hasMany(Publicacion, { foreignKey: 'id_creador', as: 'publicaciones' });
+
+const Etiqueta = require('./Etiqueta');
+
+Publicacion.belongsToMany(Etiqueta, {
+    through: 'publicacion_etiqueta',
+    foreignKey: 'id_publicacion',
+    as: 'etiquetas'
+});
+
+Etiqueta.belongsToMany(Publicacion, {
+    through: 'publicacion_etiqueta',
+    foreignKey: 'id_etiqueta',
+    as: 'publicaciones'
+});
+
+module.exports = Publicacion;
